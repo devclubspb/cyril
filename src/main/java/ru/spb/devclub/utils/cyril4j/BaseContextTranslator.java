6ab44652246c71b8cdd3dfda256430dba2da83d0
Grig Alex <alexengrigdev@gmail.com>
2020-04-23 19:31:58 +0300

    Add BaseContextTranslator

---
+++ src/main/java/ru/spb/devclub/utils/cyril4j/BaseContextTranslator.java
@@ -0,0 +1,30 @@
+package ru.spb.devclub.utils.cyril4j;
+
+/**
+ * @author Grig Alex
+ */
+public class BaseContextTranslator<T extends ContextTable> extends BaseTranslator<T> {
+    protected BaseContextTranslator(T table) {
+        super(table);
+    }
+
+    @Override
+    protected String translate(char[] symbols) {
+        if (symbols.length == 1) return translate(symbols[0]);
+        final StringBuilder builder = new StringBuilder();
+        Character previous = null;
+        Character next = null;
+        char current;
+        for (int i = 0, l = symbols.length; i < l; i++) {
+            current = symbols[i];
+            if (i + 1 < l) next = symbols[i + 1];
+            builder.append(translate(previous, current, next));
+            previous = current;
+        }
+        return builder.toString();
+    }
+
+    protected String translate(Character previous, char current, Character next) {
+        return table.get(previous, current, next);
+    }
+}
