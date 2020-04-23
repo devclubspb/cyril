459415e779515ef4ede59bcfc01ec7386d666fc8
Grig Alex <alexengrigdev@gmail.com>
2020-04-23 19:00:54 +0300

    Add Cyrillic2LatinTranslator

---
+++ src/main/java/ru/spb/devclub/utils/cyril4j/Cyrillic2LatinTranslator.java
@@ -0,0 +1,10 @@
+package ru.spb.devclub.utils.cyril4j;
+
+/**
+ * @author Grig Alex
+ */
+public class Cyrillic2LatinTranslator extends BaseTranslator {
+    protected Cyrillic2LatinTranslator(BaseCyrillic2LatinTable table) {
+        super(table);
+    }
+}
