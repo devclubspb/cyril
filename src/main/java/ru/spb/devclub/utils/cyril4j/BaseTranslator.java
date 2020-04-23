1d5515f054c561a498e5a237598df59839f3b52b
Grig Alex <alexengrigdev@gmail.com>
2020-04-23 19:00:11 +0300

    Add BaseTranslator

---
+++ src/main/java/ru/spb/devclub/utils/cyril4j/BaseTranslator.java
@@ -0,0 +1,31 @@
+package ru.spb.devclub.utils.cyril4j;
+
+/**
+ * @author Grig Alex
+ */
+public abstract class BaseTranslator implements Translator {
+    protected final Table table;
+
+    protected BaseTranslator(Table table) {
+        this.table = table;
+    }
+
+    @Override
+    public String translate(String text) {
+        if (text.isEmpty()) return text;
+        return translate(text.toCharArray());
+    }
+
+    protected String translate(char[] symbols) {
+        final StringBuilder builder = new StringBuilder();
+        for (char symbol : symbols) {
+            builder.append(translate(symbol));
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public String translate(char symbol) {
+        return table.get(symbol);
+    }
+}
